68d4eb668cf96b39b5a9700562903d86abb7cb80
ro_omelyanenko <ro_omelyanenko@betboom.org>
2022-11-12 11:24:10 +0200

    scroll to top

--- js/scrollToTop.js
+++ js/scrollToTop.js
@@ -8,31 +8,34 @@
       scrollTop.style.display = "block";
     } else {
       scrollTop.style.display = "none";
+      scrollTop.style.pointerEvents = "auto";
     }
   }
 
   scrollTop.addEventListener('click', event => {
-      scrollToTop(1000);
+    scrollTop.style.pointerEvents = "none";
+    scrollToTop(1000);
   })
 
   function scrollToTop (duration) {
-      // cancel if already on top
-      if (document.scrollingElement.scrollTop === 0) return;
+    // cancel if already on top
+    if (document.scrollingElement.scrollTop === 0) return;
 
-      const totalScrollDistance = document.scrollingElement.scrollTop;
-      let scrollY = totalScrollDistance, oldTimestamp = null;
+    const totalScrollDistance = document.scrollingElement.scrollTop;
+    let scrollY = totalScrollDistance, oldTimestamp = null;
 
-      function step (newTimestamp) {
-          if (oldTimestamp !== null) {
-              // if duration is 0 scrollY will be -Infinity
-              scrollY -= totalScrollDistance * (newTimestamp - oldTimestamp) / duration;
-              if (scrollY <= 0) return document.scrollingElement.scrollTop = 0;
-              document.scrollingElement.scrollTop = scrollY;
-          }
-          oldTimestamp = newTimestamp;
-          window.requestAnimationFrame(step);
+    function step (newTimestamp) {
+      if (oldTimestamp !== null) {
+        // if duration is 0 scrollY will be -Infinity
+        scrollY -= totalScrollDistance * (newTimestamp - oldTimestamp) / duration;
+        if (scrollY <= 0) return document.scrollingElement.scrollTop = 0;
+        document.scrollingElement.scrollTop = scrollY;
       }
+      oldTimestamp = newTimestamp;
       window.requestAnimationFrame(step);
+    }
+
+    window.requestAnimationFrame(step);
   }
 // };
 
